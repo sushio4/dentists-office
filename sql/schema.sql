a9e57ff43451f84a3befea25100042195ee85410
Maciej Suski <suskimaciej@interia.pl>
2024-12-30 21:42:31 +0100

    Add some entries to db

--- sql/schema.sql
+++ sql/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS Services (
 	ServiceID INT NOT NULL AUTO_INCREMENT,
 	ServiceName VARCHAR(100),
 	Description TEXT,
-	DurationHours INT,
+	DurationHalfHours INT,
 	Price DECIMAL(10,2),
 	PRIMARY KEY (ServiceID)
 );
@@ -40,3 +40,20 @@ CREATE TABLE IF NOT EXISTS Appointments (
 	FOREIGN KEY (StaffID) REFERENCES Staff (StaffID),
 	FOREIGN KEY (ServiceID) REFERENCES Services (ServiceID)
 );
+
+INSERT INTO Staff (FirstName, LastName, Phone, Email) VALUES
+("Jan", "Kowalski", "123123123", "jankowalski@zebex.pl"),
+("Monika", "Nowak", "321321321", "monikanowak@zebex.pl"),
+("Janusz", "Cebulski", "123456789", "januszcebulski@zebex.pl"),
+("Snoop", "Dogg", "420420420", "snoopdogg@zebex.pl");
+
+INSERT INTO Services (ServiceName, Description, DurationHalfHours, Price) VALUES
+("Kontrola", "Podstawowe badania dentystyczne", 1, 100),
+("Wyrywanie (1-2)", "Wyrwanie jednego lub dwoch zebow", 1 ,500),
+("Wyrywanie (3-5)", "Wyrwanie od trzech do pieciu zebow", 2, 1000),
+("Wyrywanie (6-10)", "Wyrwanie od szesciu do dziesieciu zebow", 3, 2000),
+("Wyrywanie (11+)", "Wyrwanie 11 lub wiecej zebow", 4, 5000),
+("Kanalowe (1kan)", "Leczenie kanalowe jednokanalowego zeba", 2, 600),
+("Kanalowe (2kan)", "Leczenie kanalowe dwukanalowego zeba", 3, 800),
+("Kanalowe (3-4kan)", "Leczenie kanalowe 3 i 4 kanalowego zeba", 4, 1200),
+("Plombowanie", "Plombowanie jednego zeba metoda xyz", 2, 600);
