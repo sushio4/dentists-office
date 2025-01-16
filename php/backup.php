f4fdcdd74d4aa8ebb3cc9d2b8e255a29e09949f2
Wiktor Janiak <wiktor.janiakk@gmail.com>
2025-01-16 14:20:50 +0100

    Database backup

--- php/backup.php
+++ php/backup.php
@@ -1,14 +1,15 @@
 <?php
+
 $DB_USER = 'root';
-$DB_PASSWORD = ''; // Password to MySQL
+$DB_PASSWORD = 'Ronaldo0709!'; // Password to MySQL
 $DB_NAME = 'dentist';
-$BACKUP_DIR = ''; // Path
+$BACKUP_DIR = 'C:\Users\wikto\Desktop\backup'; // Path
 
 $date = date('Y-m-d_H-i-s'); 
 
 $backup_file = $BACKUP_DIR . '\\' . $DB_NAME . '_' . $date . '.sql';
 
-$mysqldump_path = ''; // Path to mysqldump.exe
+$mysqldump_path = 'C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe'; // Path to mysqldump.exe
 
 $command = "\"$mysqldump_path\" -u $DB_USER -p$DB_PASSWORD $DB_NAME > \"$backup_file\"";
 
@@ -19,4 +20,6 @@ if ($return_var === 0) {
 } else {
     echo "An error occurred while performing the backup.";
 }
+// If you want the backup to be automatic, you need to configure Task Scheduler
 ?>
+
