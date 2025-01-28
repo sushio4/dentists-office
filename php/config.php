cc8d5c42dd7f24e40c0543197e2a0e188ec89626
Wiktor Janiak <wiktor.janiakk@gmail.com>
2025-01-28 18:19:04 +0100

    SSL conection(Tables don't work)

--- php/config.php
+++ php/config.php
@@ -1,12 +1,53 @@
 <?php
-define("DB_SERVER", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "dentist");
 
-$db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+function getDbConnection() {
+    try {
+        
+        $ip = "localhost";
+        $port = 3306;
+        $user = "root";
+        $password = ""; // daabase password
+        $database = "dentist";
 
-if($db->connect_error) {
-    die("ERROR: could not connect. " . $db->connect_error);
+        // SSL configuration
+        $ssl_ca = ""; //path
+        $ssl_cert = ""; //path
+        $ssl_key = ""; //path
+
+        
+        foreach ([$ssl_ca, $ssl_cert, $ssl_key] as $file) {
+            if (!file_exists($file)) {
+                throw new Exception("File does not exist: $file");
+            }
+        }
+
+        
+        $conn = mysqli_init();
+
+        if (!$conn) {
+            throw new Exception("Failed to initialise the connection.");
+        }
+
+        // SSL setup
+        mysqli_ssl_set($conn, $ssl_key, $ssl_cert, $ssl_ca, null, null);
+        
+
+        // Conect to database
+        if (!mysqli_real_connect($conn, $ip, $user, $password, $database, $port, null, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT)) {
+            throw new Exception("Connection error: " . mysqli_connect_error());
+        }
+
+        echo "Connection established!";
+        return $conn;
+
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    return null;
 }
+
+
+$db = getDbConnection();
+
 ?>
